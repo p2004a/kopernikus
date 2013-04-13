0f0ae6efc088da560aa6d328efe548f400f205c3
Marek Rusinowski <marekrusinowski@gmail.com>
2013-04-13 10:38:38 +0200

    Added information about last change in 'changes'

--- subpages/panel/changes.php
+++ subpages/panel/changes.php
@@ -7,20 +7,20 @@
       if (panel_want_name($params)) return false;
       if(!auth_check_permission("EditChanges")) return panel_no_acces_info();
       
+      $files_path = "changes";
+      $file_name = "no_file";
+      
+      $dir = opendir($files_path);
+      while($file = readdir($dir)){
+        if(($file != '.') && ($file != '..') && ($file != '.htaccess' && !is_dir($files_path . '/' . $file))){
+          $file_name = $file;
+        }
+      }
+      closedir($dir);
+      
       $form = form_load("panel_changes");
       
       if ($form && isset($_FILES['file'])) {
-      
-        $files_path = "changes";
-        $file_name = "no_file";
-        
-        $dir = opendir($files_path);
-        while($file = readdir($dir)){
-          if(($file != '.') && ($file != '..') && ($file != '.htaccess' && !is_dir($files_path . '/' . $file))){
-            $file_name = $file;
-          }
-        }
-        closedir($dir);
         
         unlink($files_path . "/" . $file_name);
         
@@ -32,13 +32,16 @@
         
         return "<h3>Zapisano zmiany</h3>";
       } else {
-        return form_create("panel_changes", "panel/changes", array(
+        $weekdays = array(1 => "Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie");
+        return array(
+        new HTMLTag("p", array(), 'Plik: ' . $file_name . '<br />Czas modyfikacji: ' . $weekdays[intval(date("N"))] . date(' Y-m-d H:i:s', filemtime($files_path . "/" . $file_name))),
+        form_create("panel_changes", "panel/changes", array(
           "MAX_FILE_SIZE" => "/10000000/"
         ), '
           <input type="hidden" name="MAX_FILE_SIZE" value="10000000" /> 
           Plik: <input type="file" name="file" /><br />
           <input type="submit" value="wyślij" class="button" />
-        ')->setAttribute("enctype", "multipart/form-data");
+        ')->setAttribute("enctype", "multipart/form-data"));
       }
     }
   }
